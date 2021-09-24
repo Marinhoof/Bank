fd0cf4d1004858ee0feb26509720e806b59fbd74
Marinhoof <44735105+Marinhoof@users.noreply.github.com>
2021-09-24 17:26:16 -0300

    Update Bank.cpp

--- Bank.cpp
+++ Bank.cpp
@@ -2,43 +2,46 @@
 
 using namespace std;
 
-
-int sub (int a, int b){
-    int saldoSaque;
-    saldoSaque=a-b;
-    return (saldoSaque);
-}
-
-int add (int a, int b){
-    int saldoSaque;
-    saldoSaque=a+b;
-    return (saldoSaque);
-}
-
-int main(){
-    int saldo = 5000;
-    int saque,deposito ,opcao ,operacao ,calc ;
-
+void mainMenu(){
     cout << "Bem vindo ao Banco";
     cout << "\n\nSelecione a opcao desejada\n";
     cout << "1.Entrar\n";
     cout << "2.Registro\n"; // not functional
     cout << "3.Sair\n";
+}
+
+int main(){
+
+    int opcao, operacao, saque, deposito;
+    double saldo=5000;
+    mainMenu();
     cin >> opcao;
 
-    if (opcao==1){
-        cout << "Qual operacao deseja realizar\n1.Saque\n2.Deposito\n3.Sair\n"; cin >> operacao;
-        if (operacao==1){
-            cout << "Insira o valor de saque: "; cin >> saque;
-            calc = sub(saldo, saque);
-            cout << "Saldo atual e : " << calc;   
-        }
+    do
+    {
+        switch (opcao)
+        {
+            
+                case 1: cout << "\nQual operacao deseja realizar\n1.Saque\t\t2.Deposito\n3.Saldo\t\t4.Sair\n" << endl; cin >> operacao;
+                switch (operacao)
+                {
+                case 1: cout << "Insira o valor de saque: R$ " ; cin >> saque;
+                saldo -= saque;
+                cout << "Seu saldo atual e R$ " << saldo;
+                }
+                switch (operacao){
+                    case 2: cout << "Insira o valor de deposito: R$ "; cin >> deposito;
+                    saldo += deposito;
+                    cout << "Seu saldo atual e R$ " << saldo;
+                }
+                switch (operacao){
+                case 3: cout << "Seu saldo e R$ "<< saldo;
+                }
+                default:
+                    break;
+            
 
-        else if (operacao==2){
-            cout << "Insira o valor de deposito: "; cin >> deposito;
-            calc = add(saldo, deposito);
-            cout << "Saldo atual e: " << calc;
-
         }
-    }
+    } while (operacao!=4);
+    
 }
